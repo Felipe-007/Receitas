9e3f6e3e39d371947067e222cc67c7f62e9c9535
Felipe Lopes da Silva <flsilva@anp.gov.br>
2022-08-29 15:03:09 -0300

    C7 - Separando os templates de home e recipe

--- recipes/views.py
+++ recipes/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'global/base.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html')
