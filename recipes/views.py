514a8e7dafb06ca9b58d2ffa0e085fdea6bfb32a
Felipe Lopes da Silva <flsilva@anp.gov.br>
2022-08-29 16:49:59 -0300

    C8 - Herança em templates e template blocks

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'global/base.html')
+    return render(request, 'recipes/pages/home.html')
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html')
