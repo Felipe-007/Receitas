6c330fb08231559e5fe02ec64e530013e8834802
Felipe Lopes da Silva <flsilva@anp.gov.br>
2022-09-14 11:44:39 -0300

    C18 - Usando o model Recipe real (com QuerySet) na view home

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,5 @@
 # tabela de dados
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -11,25 +12,29 @@ class Category(models.Model):
 
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=65)
-    description = models.CharField(max_length=165)
+    title = models.CharField(max_length=65, verbose_name="Titulo")
+    description = models.CharField(max_length=165, verbose_name="Descrição")
     slug = models.SlugField()
-    preparation_time = models.IntegerField()
+    preparation_time = models.IntegerField(verbose_name="Tempo de preparação")
     preparation_time_unit = models.CharField(max_length=65)
-    servings = models.IntegerField()
+    servings = models.IntegerField(verbose_name="Porções")
     servings_unit = models.CharField(max_length=65)
-    preparation_steps = models.TextField()
-    preparation_steps_is_html = models.BooleanField(default=False)
+    preparation_steps = models.TextField(verbose_name="Etapas de preparação")
+    preparation_steps_is_html = models.BooleanField(
+        default=False, verbose_name="Etapas de preparação HTML")
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateField(auto_now=True)
-    is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
-    # category = faz a relação emtre caregoria e as receitas
+    is_published = models.BooleanField(default=False, verbose_name="Publicado")
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='',
+        verbose_name="Imagem")
+    # category = faz a relação entre caregoria e as receitas
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True)
+        Category, on_delete=models.SET_NULL, null=True,
+        verbose_name="Categoria")
 
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True)
+        User, on_delete=models.SET_NULL, null=True, verbose_name="Autor")
 
     def __str__(self):
         return self.title  # faz aparece o nome da receita
