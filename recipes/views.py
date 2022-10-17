4242ea4f6fbea8c3ebe7408dff2ac1dac1cb5b63
Felipe Lopes da Silva <flsilva@anp.gov.br>
2022-10-17 13:19:10 -0300

    C35 - Filtro de campo contains e icontains e consultas complexas com Q

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.recipes.factory import make_recipe
 from .models import Recipe
 from django.http.response import Http404
+from django.db.models import Q
 
 
 def home(request):
@@ -32,7 +33,7 @@ def recipe(request, id):
         'recipe': recipe,
         'is_detail_page': True,
     })
-    
+
 
 def search(request):
     search_term = request.GET.get('q', '').strip()
@@ -40,7 +41,13 @@ def search(request):
     if not search_term:
         raise Http404()
 
+    recipes = Recipe.objects.filter(
+        Q(title__icontains=search_term) |
+        Q(description__icontains=search_term),
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
