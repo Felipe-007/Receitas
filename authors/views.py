c716d421663f44df02fe222db966ff90b2ae6713
Felipe Lopes da Silva <flsilva@anp.gov.br>
2022-11-18 13:08:38 -0300

    C49 - is_valid e save para salvar dados do formulário

--- authors/views.py
+++ authors/views.py
@@ -1,10 +1,11 @@
 from django.http import Http404
 from django.shortcuts import redirect, render
+from django.contrib import messages
 
 from .forms import RegisterForm
 
 
-def register_view(request):
+def register_view(request):    
     register_form_data = request.session.get('register_form_data', None)
     form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
@@ -19,5 +20,11 @@ def register_create(request):
     POST = request.POST
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
+
+    if form.is_valid():
+        form.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del(request.session['register_form_data'])
 
     return redirect('authors:register')
